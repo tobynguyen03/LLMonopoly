63159c5a656d11f86227e08ac7320acf39bd87b5
StanleyWong <stanley0620@gmail.com>
2024-11-19 20:34:13 -0500

    added qwen agent

---
+++ game/qwen_agent.py
@@ -0,0 +1,25 @@
+import requests
+import json
+class Qwen_Agent():
+    def __init__(self):
+        # Uses LLama3.2 via ollama, need to have ollama running for API to work
+        self.url = "http://localhost:11434/api/chat"
+    def query(self, prompt):
+        data = {
+            "model": "qwen2.5:7b",
+            "messages": [
+                {
+                "role": "user",
+                "content": prompt
+                }
+            ],
+            "stream": False
+        }
+        
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        
+        response = requests.post(self.url, headers=headers, json=data)
+        # return (response.json())
+        return(response.json()['message']['content'])

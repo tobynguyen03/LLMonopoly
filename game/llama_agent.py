7dd7da56de46755bc1be6bbd3e747d8502bdea96
tobynguyen03 <toby.nguyen03@gmail.com>
2024-11-19 19:54:28 -0500

    add logging for game end and phi3

--- game/llama_agent.py
+++ game/llama_agent.py
@@ -1,9 +1,12 @@
 import requests
 import json
+
 class Llama3_Agent():
     def __init__(self):
         # Uses LLama3.2 via ollama, need to have ollama running for API to work
         self.url = "http://localhost:11434/api/chat"
+        self.name = "Llama-3.2"
+
     def query(self, prompt):
         data = {
             "model": "llama3.2",
